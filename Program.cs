e309676513ed3bb08230c85eed02ed1d208d49dd
cpond2276 <cpond2276@gmail.com>
2026-07-25 22:49:28 -0600

    First comment and user instutions

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Carson Pond 7/25/2026 Lab 8: Maze part I
+
+Console.Clear();
+Console.WriteLine("Welcome to the Maze!!");
+Console.WriteLine("Use the arrow keys to move around the maze");
+Console.WriteLine("Reach the * to escape, good luck!!");
+
