c60fb678dd8a69b2fd302c1bafdf6b5c911b703c
cpond2276 <cpond2276@gmail.com>
2026-07-25 23:21:28 -0600

    cursor moves around most of the places

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿// Carson Pond 7/25/2026 Lab 8: Maze part I
 
+// clearing console and giving user instuctions
 Console.Clear();
 Console.WriteLine("Welcome to the Maze!!");
 Console.WriteLine("Use the arrow keys to move around the maze");
-Console.WriteLine("Reach the (*) to escape. Good luck!!");
-Console.WriteLine(" ");
+Console.WriteLine("Reach the (*) to escape or hit the 'escape' key to quit");
+Console.WriteLine("Press any key to begin and good luck!!");
+Console.ReadKey(true); // change from instuctions to map
 
+Console.Clear();
+// loading in and printing the map
 string[] mapRows = System.IO.File.ReadAllLines("map.txt");
 
 for (int i = 0; i < mapRows.Length; i++)
@@ -13,3 +17,33 @@ for (int i = 0; i < mapRows.Length; i++)
     Console.WriteLine(mapRows[i]);
 }
 
+//begining of gameplay
+Console.SetCursorPosition(0, 0); // staring position set
+
+ConsoleKey key;
+
+do // arrows actually move you around!!
+{
+    key = Console.ReadKey(true).Key;
+
+    if (key == ConsoleKey.Escape)// giving up the maze :(
+    {
+        return;
+    }
+    else if (key == ConsoleKey.UpArrow)
+    {
+        Console.CursorTop--;
+    }
+    else if (key == ConsoleKey.DownArrow)
+    {
+        Console.CursorTop++;
+    }
+    else if (key == ConsoleKey.LeftArrow)
+    {
+        Console.CursorLeft--;
+    }
+    else if (key == ConsoleKey.RightArrow)
+    {
+        Console.CursorLeft++;
+    }
+} while (true);
--- obj/Debug/net10.0/lab-8-maze-cpond2276.AssemblyInfo.cs
+++ obj/Debug/net10.0/lab-8-maze-cpond2276.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-8-maze-cpond2276")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e309676513ed3bb08230c85eed02ed1d208d49dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eab56e1466171f647d93aec04959be38e0c8c56a")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-8-maze-cpond2276")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-8-maze-cpond2276")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
